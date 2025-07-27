0dc194a2be03099b0d29339dbaeee99aa3ca054f
devsk <srik3713@gmail.com>
2025-07-27 12:39:54 +0530

    application tested

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -84,6 +84,7 @@ const HomePage = () => {
       {/* Filters */}
       <div className="card p-4 mb-6">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+
           <div className="relative">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
             <input
